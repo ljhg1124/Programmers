a9e46a4164c7240cfcc250390fd5ba8d52e2c3df
Jake <ljhg1124@gmail.com>
2019-10-04 01:46:02 +0900

    연습문제 행렬의 덧셈

---
+++ Level1/연습문제_행렬의 덧셈/Level1_Exercise28/Level1_Exercise28/Main.cpp
@@ -0,0 +1,63 @@
+#include "Main.h"
+
+/*
+행렬의 덧셈
+
+문제 설명
+행렬의 덧셈은 행과 열의 크기가 같은 두 행렬의 같은 행, 같은 열의 값을 서로 더한 결과가 됩니다. 2개의 행렬 arr1과 arr2를 입력받아, 행렬 덧셈의 결과를 반환하는 함수, solution을 완성해주세요.
+
+제한 조건
+행렬 arr1, arr2의 행과 열의 길이는 500을 넘지 않습니다.
+
+입출력 예
+
+arr1			arr2			return
+[[1,2],[2,3]]	[[3,4],[5,6]]	[[4,6],[7,9]]
+[[1],[2]]		[[3],[4]]		[[4],[6]]
+*/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
+	vector< vector<int> > answer;
+	
+	for (int i = 0; i < arr1.size(); i++)
+	{
+		vector<int> num;
+		for (int j = 0; j < arr1[i].size(); j++)
+		{
+			num.push_back(arr1[i][j] + arr2[i][j]);
+		}
+		answer.push_back(num);
+	}
+
+	/*
+	vector< vector<int> > answer = arr1;
+	
+	for (int i = 0; i < answer.size(); i++)
+	{
+		for (int j = 0; j < answer[i].size(); j++)
+		{
+			answer[i][j] = arr1[i][j] + arr2[i][j];
+		}
+	}
+	*/
+	
+	return answer;
+}
+
+int main(void)
+{
+	//vector<vector<int>> arr1 = { {1,2}, {2,3} };
+	//vector<vector<int>> arr2 = { {3,4}, {5,6} };
+
+	vector<vector<int>> arr1 = { {1,2} };
+	vector<vector<int>> arr2 = { {3,4} };
+
+	solution(arr1, arr2);
+
+	return 0;
+}
