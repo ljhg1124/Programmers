00d458a270b6c4ddf67ec303d361803ee9fd7d26
Jun <ljhg1124@gmail.com>
2021-04-28 10:43:23 +0900

    주식가격

---
+++ 코딩테스트_연습/C#/스택_큐/주식가격/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace 주식가격
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] test = { 1, 2, 3, 2, 3 };
+
+            solution(test);
+            Console.WriteLine("Hello World!");
+        }
+
+        static int[] solution(int[] prices)
+        {
+            int[] answer = new int[prices.Length];
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                for (int j = i + 1; j < prices.Length; j++)
+                {
+                    answer[i]++;
+                    if (prices[i] > prices[j])
+                    {
+                        break;
+                    }
+                }
+
+                for (int d = 0; d < answer.Length; d++)
+                {
+                    Console.Write(answer[d] + " ");
+                }
+                Console.WriteLine("\n--------------");
+            }
+            return answer;
+        }
+    }
+}
