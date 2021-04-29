ccbdca45aab48809f8d9d6631dbce57262fd2a19
Jun <ljhg1124@gmail.com>
2021-04-29 16:09:51 +0900

    기능개발

---
+++ 코딩테스트_연습/C#/스택_큐/기능개발/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace 기능개발
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] progresses = { 93, 30, 55 };
+            int[] speeds = { 1, 30, 5 };
+
+            solution(progresses, speeds);
+
+            Console.WriteLine("Hello World!");
+        }
+        static public int[] solution(int[] progresses, int[] speeds)
+        {
+            Queue<int> result = new Queue<int>();
+            Queue<int> rate = new Queue<int>();
+
+            for (int i = 0; i < progresses.Length; i++)
+            {
+                rate.Enqueue(countDay(progresses[i], speeds[i]));
+            }
+
+            int count = 0;
+            int cur = rate.Peek();
+
+            while (rate.Count != 0)
+            {
+                if (cur < rate.Peek())
+                {
+                    result.Enqueue(count);
+                    cur = rate.Peek();
+                    count = 0;
+                }
+                count++;
+                rate.Dequeue();
+            }
+
+            result.Enqueue(count);
+            return result.ToArray();
+        }
+
+        static int countDay(int progresses, int speeds)
+        {
+            int persent = progresses;
+            int count = 0;
+            while (persent < 100)
+            {
+                persent += speeds;
+                count++;
+            }
+
+            return count;
+        }
+    }
+}
